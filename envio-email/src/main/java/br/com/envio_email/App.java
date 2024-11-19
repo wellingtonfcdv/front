aaa47dd04406039c027d8e9fe2a8d39109b2eb6e
Wellington Feijo <p-wcosta@tce.sp.gov.br>
2024-11-19 14:15:05 -0300

    Configuração das propriedades do smtp

--- envio-email/src/main/java/br/com/envio_email/App.java
+++ envio-email/src/main/java/br/com/envio_email/App.java
@@ -1,13 +1,25 @@
 package br.com.envio_email;
 
-/**
- * Hello world!
- *
- */
+import java.util.Properties;
+
+import org.junit.Test;
+
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	@Test
+    public void testeEmail() {
+    	/*
+    	 * wellinfeijo.com.br@gmail.com
+    	 * 
+    	 * 
+    	 * */
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true");/*Autorização*/
+		properties.put("mail.smtp.starttis", "true");/*Autenticação*/
+		properties.put("mail.smtp.host", "smtp.gmail.com");/*Servidor gmail Google*/
+		properties.put("mail.smtp.host", "465");/*Porta do servidor*/
+		properties.put("mail.smtp.socketFactory.port", "465");/*Especifica a porta a ser conectada pelo socket*/
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");/*Classe socket de conexão ao smtp*/
     }
 }
